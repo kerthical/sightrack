81fc26defce47525422225f0a71be89bc51a317e
kerthical <121681466+kerthical@users.noreply.github.com>
2023-11-28 02:22:43 +0900

    feat: setup linting and formatting

---
+++ frontend/vite.config.ts
@@ -0,0 +1,54 @@
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import viteReact from '@vitejs/plugin-react';
+import cssnano from 'cssnano';
+import { resolve } from 'path';
+import mantinePreset from 'postcss-preset-mantine';
+import simpleVars from 'postcss-simple-vars';
+import tailwindcss from 'tailwindcss';
+import { defineConfig } from 'vite';
+
+const root = resolve('src');
+export default defineConfig(() => ({
+  plugins: [viteReact(), vanillaExtractPlugin()],
+  server: {
+    port: 1420,
+    strictPort: true,
+  },
+  root: root,
+  resolve: {
+    alias: {
+      '@': root,
+    },
+  },
+  build: {
+    outDir: resolve('dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(root, 'index.html'),
+      },
+    },
+  },
+  esbuild: {
+    legalComments: 'none' as const,
+  },
+  css: {
+    postcss: {
+      plugins: [
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        mantinePreset(),
+        simpleVars({
+          variables: {
+            'mantine-breakpoint-xs': '36em',
+            'mantine-breakpoint-sm': '48em',
+            'mantine-breakpoint-md': '62em',
+            'mantine-breakpoint-lg': '75em',
+            'mantine-breakpoint-xl': '88em',
+          },
+        }),
+        tailwindcss(),
+        cssnano(),
+      ],
+    },
+  },
+}));
