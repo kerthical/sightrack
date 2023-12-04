be043f24189262cb99753eb53a072935bc4a30c5
kerthical <121681466+kerthical@users.noreply.github.com>
2023-12-04 17:58:10 +0900

    refactor: output styles

--- backend/src/cli.py
+++ backend/src/cli.py
@@ -16,19 +16,19 @@ def process_file(input: str, output: str):
         image = cv2.imread(input)
         width = image.shape[1]
         height = image.shape[0]
-        print(f"[+] Image file opened with {width}x{height} resolution")
+        print(f"[+] Image file {os.path.abspath(input)} opened with {width}x{height} resolution")
 
         result = processor.process_frame(image)
 
         if output.split(".")[-1] in ["jpg", "jpeg", "png"]:
             cv2.imwrite(output, image)
-            print("[+] Image file processed and saved")
+            print(f"[+] Image file processed and result saved to {os.path.abspath(output)}")
         elif output.split(".")[-1] in ["csv"]:
             with open(output, "w") as f:
                 f.write(
                     f"frame,detected,score,yaw,pitch,roll\n{result.detected},{result.box.score},{result.rotation.yaw},{result.rotation.pitch},{result.rotation.roll}\n"
                 )
-            print("[+] Image file processed and data saved to csv")
+            print(f"[+] Image file processed and data saved to {os.path.abspath(output)}")
         else:
             print("[!] Unsupported output file format")
             sys.exit(1)
@@ -36,14 +36,14 @@ def process_file(input: str, output: str):
     elif file_extension in ["mp4", "avi", "mov"]:
         capture: cv2.VideoCapture = cv2.VideoCapture(input)
         if not capture.isOpened():
-            print("[!] Failed to open video file")
+            print(f"[!] Failed to open video file {os.path.abspath(input)}")
             sys.exit(1)
         fps: int = int(capture.get(cv2.CAP_PROP_FPS))
         width: int = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
         height: int = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
         total_frames: int = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
         print(
-            f"[+] Video file opened with {fps} fps, {width}x{height} resolution and {total_frames} frames"
+            f"[+] Video file {os.path.abspath(input)} opened with {fps} fps, {width}x{height} resolution and {total_frames} frames"
         )
 
         frame_count: int = 0
@@ -60,7 +60,7 @@ def process_file(input: str, output: str):
                 outfile.write(result.image)
                 frame_count += 1
                 print(f"\r[+] Processing frame {frame_count+1}/{total_frames} ({round((frame_count+1)/total_frames*100, 2)}%)", end="")
-            print("[+] Video file processed and saved")
+            print(f"[+] Video file processed and saved to {os.path.abspath(output)}")
         elif output.split(".")[-1] in ["csv"]:
             with open(output, "w") as f:
                 f.write("frame,detected,score,yaw,pitch,roll\n")
@@ -72,7 +72,7 @@ def process_file(input: str, output: str):
                     f.write(f"{frame_count},{result.detected},{result.box.score},{result.rotation.yaw},{result.rotation.pitch},{result.rotation.roll}\n")
                     frame_count += 1
                     print(f"\r[+] Processing frame {frame_count+1}/{total_frames} ({round((frame_count+1)/total_frames*100, 2)}%)", end="")
-            print("[+] Video file processed and data saved to csv")
+            print(f"[+] Video file processed and data saved to {os.path.abspath(output)}")
         else:
             print("[!] Unsupported output file format")
             sys.exit(1)
@@ -91,11 +91,11 @@ if __name__ == "__main__":
     args: argparse.Namespace = parser.parse_args()
 
     if not os.path.exists(args.input) or os.path.isdir(args.input):
-        print("[!] Input file does not exist")
+        print(f"[!] Input file {os.path.abspath(args.input)} does not exist")
         sys.exit(1)
 
     if os.path.exists(args.output) and os.path.isdir(args.output):
-        print("[!] Output file is a directory")
+        print(f"[!] Output file {os.path.abspath(args.output)} is a directory")
         sys.exit(1)
 
     if os.path.exists(args.output) and not args.force:
